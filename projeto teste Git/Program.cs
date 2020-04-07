a6e1b7a2c294a26722827ab72b46a71d008bb845
Osmar <osmar570@outlook.com>
2020-04-07 03:31:54 -0300

    Adicionado para mostrar a mensagem de bom dia

--- projeto teste Git/Program.cs
+++ projeto teste Git/Program.cs
@@ -7,6 +7,7 @@ namespace projeto_teste_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
